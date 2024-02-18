f3333a1a398302bc4f6259e04373be4badf005b9
AJ Harris <shogunaut90@gmail.com>
2024-02-18 12:40:58 -0600

    A rewritten hasDuplicateId test using the DRY principle and hooks with the beforeEach and afterEach functions

--- hasduplicateids/hasduplicateids.test.js
+++ hasduplicateids/hasduplicateids.test.js
@@ -1,6 +1,24 @@
+// Running these test agains however this time with the DRY Principle using hooks
+
 const hasDuplicateIds = require('./hasduplicateids');
 
 describe('DOM Tree has Duplicate IDs', () => {
+  let root;
+  beforeEach(() => {
+    console.log('beforeEach ran...');
+    // Create mock elements
+    root = document.createElement('div');
+    const child1 = document.createElement('div');
+    const child2 = document.createElement('div');
+
+    // Append child elements
+    root.appendChild(child1);
+    root.appendChild(child2);
+  });
+  afterEach(() => {
+    console.log('afterEach ran...');
+    root = null;
+  });
   it('should be a function', () => {
     expect(typeof hasDuplicateIds).toEqual('function');
   });
@@ -14,19 +32,11 @@ describe('DOM Tree has Duplicate IDs', () => {
   });
 
   it('should return true if there are duplicate IDs', () => {
-    // Create mock elements
-    const root = document.createElement('div');
-    const child1 = document.createElement('div');
-    const child2 = document.createElement('div');
-
-    // Append child elements
-    root.appendChild(child1);
-    root.appendChild(child2);
 
     // Add duplicate IDs
     root.id = 'root';
-    child1.id = 'child';
-    child2.id = 'child';
+    root.children[0].id = 'child';
+    root.children[1].id = 'child';
 
     const result = hasDuplicateIds(root);
 
@@ -34,19 +44,11 @@ describe('DOM Tree has Duplicate IDs', () => {
   });
 
   it('should return false if there are no duplicate IDs', () => {
-    // Create mock elements
-    const root = document.createElement('div');
-    const child1 = document.createElement('div');
-    const child2 = document.createElement('div');
-
-    // Append child elements
-    root.appendChild(child1);
-    root.appendChild(child2);
 
     // Add duplicate IDs
     root.id = 'root';
-    child1.id = 'child1';
-    child2.id = 'child2';
+    root.children[0].id = 'child1';
+    root.children[1].id = 'child2';
 
     const result = hasDuplicateIds(root);
 
